2e6bc76b3f8155fb0ee615c526615ca605093c89
P Ritoranta <p.ritoranta@proton.me>
2025-07-27 14:29:22 +0300

    chore: remove certain warnings from vs code and builds

--- svelte.config.js
+++ svelte.config.js
@@ -4,4 +4,14 @@ export default {
     // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
     // for more information about preprocessors
     preprocess: vitePreprocess(),
+    onwarn: (warning, handler) => {
+        // ignore certain warnings during build
+        if (
+            warning.code === 'a11y_consider_explicit_label' ||
+            warning.code === 'a11y_missing_attribute'
+        )
+            return
+        // show rest of the warnings as normal
+        handler(warning)
+    },
 }
